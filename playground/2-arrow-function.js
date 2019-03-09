afc61fe0ae027979b39becba0bb3944b00348b2a
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-09 17:32:54 -0600

    Arrow Functions explanation

---
+++ playground/2-arrow-function.js
@@ -0,0 +1,58 @@
+// Old syntax
+// const square = function(x) {
+//   return x * x
+// }
+// ES6
+// const square = x => {
+//   return x * x
+// }
+// Shorthand
+// const square = x => x * x
+
+// console.log(square(3))
+// ----------------------------------
+// const event = {
+//   name: 'Birthday Party',
+//   printGuestList: function() {
+//     console.log('Guest list for ' + this.name)
+//     // returns this.name undefined
+//   }
+// }
+
+// const event = {
+//   name: 'Birthday Party',
+//   printGuestList: () => {
+//     console.log('Guest list for ' + this.name)
+//     // returns this.name undefined
+//   }
+// }
+// const event = {
+//   name: 'Birthday Party',
+//   printGuestList: () => {
+//     console.log('Guest list for ' + this.name)
+//     // returns this.name undefined
+//   }
+// }
+
+// ES6 function for defining methods on objects definition with this binding
+const event = {
+  name: 'Birthday Party',
+  guestList: ['John', 'Jen', 'Mike'],
+  printGuestList() {
+    console.log('Guest list for ' + this.name)
+    // this.guestList.forEach(function(guest) {
+    //   console.log(guest + ' is attending ' + this.name)
+    //   // returns John is attending undefined
+    // })
+    this.guestList.forEach(guest => {
+      console.log(guest + ' is attending ' + this.name)
+      // returns John is attending undefined
+    })
+  }
+}
+
+// Arrow functions don't bind their own this value
+// that makes them poor candidates for methods
+// but great candidates for everything else
+
+event.printGuestList()
