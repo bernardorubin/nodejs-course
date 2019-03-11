1c89266f1d23896c93afef73136c75624f41ff08
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-10 23:23:58 -0600

    Removed comments

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,26 +1,29 @@
-// setTimeout(() => {
-//     console.log('Two seconds are up')
-// }, 2000)
+// this is a callback and is asynchronous
+// setTimeout is a node provided API
+setTimeout(() => {
+  console.log('Two seconds are up')
+}, 2000)
+// a callback is a function we provide to another function as an argument with the intention of having it called later on
+// forEach and filter use callback pattern but are not asynchronous
+const names = ['Andrew', 'Jen', 'Jess']
+const shortNames = names.filter(name => {
+  return name.length <= 4
+})
 
-// const names = ['Andrew', 'Jen', 'Jess']
-// const shortNames = names.filter((name) => {
-//     return name.length <= 4
-// })
+const geocode = (address, callback) => {
+  setTimeout(() => {
+    const data = {
+      latitude: 0,
+      longitude: 0
+    }
 
-// const geocode = (address, callback) => {
-//     setTimeout(() => {
-//         const data = {
-//             latitude: 0,
-//             longitude: 0
-//         }
+    callback(data)
+  }, 2000)
+}
 
-//         callback(data)
-//     }, 2000)
-// }
-
-// geocode('Philadelphia', (data) => {
-//     console.log(data)
-// })
+geocode('Philadelphia', data => {
+  console.log(data)
+})
 
 const add = (a, b, callback) => {
   setTimeout(() => {
