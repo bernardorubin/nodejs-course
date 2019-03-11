12bf101025592860a5fb7d0e1a1f23ab4566ef2b
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-11 17:28:53 -0600

    Refactored using property shorthand and destructuring

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -8,19 +8,19 @@ const forecast = (latitude, longitude, callback) => {
     longitude +
     '?units=si&lang=es'
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service!', undefined)
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback('Unable to find location', undefined)
     } else {
       callback(
         undefined,
-        response.body.daily.data[0].summary +
+        body.daily.data[0].summary +
           ' It is currently ' +
-          response.body.currently.temperature +
+          body.currently.temperature +
           ' degrees out there is a ' +
-          response.body.currently.precipProbability +
+          body.currently.precipProbability +
           '% chance of rain'
       )
     }
