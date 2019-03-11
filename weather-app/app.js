4494d20f98d182a9ca2746cb284dc73613775e96
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-11 16:06:31 -0600

    Callback Chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,15 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Mexico City', (error, data) => {
-  console.log('Error: ', error)
-  console.log('Data: ', data)
-})
-
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error:', error)
-  console.log('Data:', data)
+geocode('Queretaro', (error, data) => {
+  if (error) {
+    return console.log(error)
+  }
+  forecast(data.latitude, data.longitude, (error, forecastData) => {
+    if (error) {
+      return console.log(error)
+    }
+    console.log(data.location)
+    console.log(forecastData)
+  })
 })
