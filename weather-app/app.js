01676ee42d415f908758128342541b9a3cb4833e
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-11 00:54:32 -0600

    Abstracted geocode callback

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,5 @@
 const request = require('request')
-
+const geocode = require('./utils/geocode')
 const url =
   'https://api.darksky.net/forecast/1c6718a83ce0b425d1f779d8e406db74/37.8267,-122.4233?units=si&lang=es'
 
@@ -20,17 +20,22 @@ request({ url: url, json: true }, (error, response) => {
   }
 })
 
-const geocodeURL =
-  'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmVybmFyZG9ydWJpbiIsImEiOiJjanQzcDZpcXkwdGNtNGFwYzFsY2V4NnBhIn0.RNGuEJDUebMgSd1d0T_1UQ&limit=1'
+// const geocodeURL =
+//   'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmVybmFyZG9ydWJpbiIsImEiOiJjanQzcDZpcXkwdGNtNGFwYzFsY2V4NnBhIn0.RNGuEJDUebMgSd1d0T_1UQ&limit=1'
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to location services!')
-  } else if (response.body.features.length === 0) {
-    console.log('Unable to find location. Try another search.')
-  } else {
-    const longitude = response.body.features[0].center[0]
-    const latitude = response.body.features[0].center[1]
-    console.log(latitude, longitude)
-  }
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//   if (error) {
+//     console.log('Unable to connect to location services!')
+//   } else if (response.body.features.length === 0) {
+//     console.log('Unable to find location. Try another search.')
+//   } else {
+//     const longitude = response.body.features[0].center[0]
+//     const latitude = response.body.features[0].center[1]
+//     console.log(latitude, longitude)
+//   }
+// })
+
+geocode('Mexico City', (error, data) => {
+  console.log('Error: ', error)
+  console.log('Data: ', data)
 })
