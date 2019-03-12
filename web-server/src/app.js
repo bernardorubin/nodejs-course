7ff1c3d9c9bd8c3f0f9083572022d56458bd049a
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-11 19:44:57 -0600

    Added html files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,28 @@
+const path = require('path')
 const express = require('express')
 
+console.log(__dirname)
+console.log(__filename)
+console.log(path.join(__dirname, '../public'))
+
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('/', (req, res) => {
-  res.send('<h1>Weather</h1>')
-})
+app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-  // express automatically detects we are sending an object
-  // and automatically parses it and converts it to JSON
-  res.send([{ name: 'Andrew', age: 27 }, { name: 'Bern', age: 29 }])
-})
+// app.get('', (req, res) => {
+//   res.send('<h1>Weather</h1>')
+// })
 
-app.get('/about', (req, res) => {
-  res.send('<h1>About</h1>')
-})
+// app.get('/help', (req, res) => {
+//   // express automatically detects we are sending an object
+//   // and automatically parses it and converts it to JSON
+//   res.send([{ name: 'Andrew', age: 27 }, { name: 'Bern', age: 29 }])
+// })
+
+// app.get('/about', (req, res) => {
+//   res.send('<h1>About</h1>')
+// })
 
 app.get('/weather', (req, res) => {
   console.log(req)
