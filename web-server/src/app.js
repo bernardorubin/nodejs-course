f396d7a212da29c5bd556587d7546c77ec4f1c08
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-11 20:03:53 -0600

    Set up handlebars plugin hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,6 +8,7 @@ console.log(path.join(__dirname, '../public'))
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
 // app.get('', (req, res) => {
