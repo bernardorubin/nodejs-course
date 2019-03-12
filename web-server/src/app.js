38898ad472197cde228e440a54be4c5ef3ab54b6
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-11 19:21:19 -0600

    added new routes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
   res.send('Hello from express')
 })
 
+app.get('/help', (req, res) => {
+  res.send('Help page')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About page')
+})
+
+app.get('/weather', (req, res) => {
+  console.log(req)
+  res.send('Weather page')
+})
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
 })
