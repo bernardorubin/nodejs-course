ff77f32e4df5370de639ae417b682fe10a329b52
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-13 14:01:14 -0600

    Understand the ObjectID from mongodb

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,11 +1,21 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb')
-
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+// same imports as above but with destructuring:
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
+console.log(id.id.length)
+console.log(id.toHexString().length)
+// mongo stores ids as binary data because the size compared to a string is half
+// it gets saved as the value returned by a call to the function ObjectId("263253sdc6823681848164asd12") with returns the string in binary
 
 MongoClient.connect(
   connectionURL,
@@ -16,18 +26,19 @@ MongoClient.connect(
     }
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne(
-    //   {
-    //     name: 'Bern',
-    //     age: 29
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log('Unable to insert user')
-    //     }
-    //     console.log(result.ops)
-    //   }
-    // )
+    db.collection('users').insertOne(
+      {
+        _id: id,
+        name: 'Chu',
+        age: 30
+      },
+      (error, result) => {
+        if (error) {
+          return console.log('Unable to insert user')
+        }
+        console.log(result.ops)
+      }
+    )
 
     // db.collection('users').insertMany(
     //   [
@@ -48,20 +59,23 @@ MongoClient.connect(
     //   }
     // )
 
-    db.collection('tasks').insertMany(
-      [
-        {
-          description: 'This is a description',
-          completed: false
-        },
-        { description: 'This is a task', completed: true }
-      ],
-      (error, response) => {
-        if (error) {
-          return console.log('Unable to insert tasks')
-        }
-        console.log(response.ops)
-      }
-    )
+    // db.collection('tasks').insertMany(
+    //   [
+    //     {
+    //       description: 'This is a description',
+    //       completed: false
+    //     },
+    //     {
+    //       description: 'This is a task that has been completed',
+    //       completed: true
+    //     }
+    //   ],
+    //   (error, response) => {
+    //     if (error) {
+    //       return console.log('Unable to insert tasks')
+    //     }
+    //     console.log(response.ops)
+    //   }
+    // )
   }
 )
