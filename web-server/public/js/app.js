4c9c3f079f212f369eef856c14fe283fb9171e69
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-12 20:52:00 -0600

    Browser Http requests with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Client side javascript file loaded!')
+// to make http request from client side javascript
+// we'll use popular fetch api -> browser based api
+// we can use it in all modern browsers but not accessible
+// in nodejs. can't be used on a backend node script
+
+fetch('http://localhost:3000/weather?address=boston').then(response => {
+  response.json().then(data => {
+    data.error
+      ? console.log(data.error)
+      : console.log(data.forecast, data.location)
+  })
+})
