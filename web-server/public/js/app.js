9a4aaa5ccc1879729aab65a62975acb1fe28a5fd
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-12 21:52:42 -0600

    Wired up user interface to show errors and weather

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -4,23 +4,27 @@ console.log('Client side javascript file loaded!')
 // we can use it in all modern browsers but not accessible
 // in nodejs. can't be used on a backend node script
 
-const fetchData = address => {
-  fetch('http://localhost:3000/weather?address=' + address).then(response => {
-    response.json().then(data => {
-      data.error
-        ? console.log(data.error)
-        : console.log(data.forecast, data.location)
-    })
-  })
-}
-
 const weatherForm = document.querySelector('form')
-
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', e => {
   e.preventDefault()
   const location = search.value
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
   fetchData(location)
-  console.log(location)
 })
+
+const fetchData = address => {
+  fetch('http://localhost:3000/weather?address=' + address).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        return (messageOne.textContent = data.error)
+      }
+      messageOne.textContent = data.location
+      messageTwo.textContent = data.forecast
+    })
+  })
+}
