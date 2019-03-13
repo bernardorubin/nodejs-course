00f012aa44339fcdaf9eab4ec028a373e927ec8b
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-12 19:02:25 -0600

    Wired up endpoint to geocode and forecast

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 // nodemon src/app.js -e js,hbss
 console.log(__dirname)
@@ -56,10 +58,21 @@ app.get('/weather', (req, res) => {
       error: 'You must provide an address'
     })
   }
-  res.send({
-    forecast: 'something',
-    location: 'something',
-    address: req.query.address
+
+  geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.send({ error })
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error })
+      }
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address
+      })
+    })
   })
 })
 
