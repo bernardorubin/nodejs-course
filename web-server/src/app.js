adfd9ee79cd4b62bb5917ceebf0c83b7816fe79f
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-12 18:10:52 -0600

    Composed response for query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -50,10 +50,27 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-  console.log(req)
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address'
+    })
+  }
   res.send({
     forecast: 'something',
-    location: 'something'
+    location: 'something',
+    address: req.query.address
+  })
+})
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    })
+  }
+  console.log(req.query.search)
+  res.send({
+    products: []
   })
 })
 
