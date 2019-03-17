05405d4dcc8b9fb55d65c0fab2d840704d11d29d
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-17 17:11:27 -0600

    Removed blank line

--- 4-task-manager/src/index.js
+++ 4-task-manager/src/index.js
@@ -4,7 +4,6 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-
 const port = process.env.PORT || 3000 // Get PORT value for heroku deployment from process.env.PORT
 
 app.use(express.json()) // Automatically parses incoming json to an object so we can access it in our request handlers
