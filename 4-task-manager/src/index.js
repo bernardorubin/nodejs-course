b21030735d3b9837c748bcad122a5fca849112c2
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-17 16:33:36 -0600

    Converted route functions to use async await

--- 4-task-manager/src/index.js
+++ 4-task-manager/src/index.js
@@ -10,68 +10,105 @@ const port = process.env.PORT || 3000
 // Automatically parses incoming json to an object so we can access it in our request handlers
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body)
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user)
-    })
-    .catch(error => {
-      // res.status(400)
-      // res.send(error)
-      // In One line ->
-      // res.send(error, 400)
-      res.status(400).send(error)
-    })
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+  // user
+  //   .save()
+  //   .then(() => {
+  //     res.status(201).send(user)
+  //   })
+  //   .catch(error => {
+  //     // res.status(400)
+  //     // res.send(error)
+  //     // In One line ->
+  //     // res.send(error, 400)
+  //     res.status(400).send(error)
+  //   })
 })
 
-app.get('/users', (req, res) => {
-  // Fetch all users via promise that find returns
-  User.find({})
-    .then(users => {
-      res.status(200).send(users)
-    })
-    .catch(error => {
-      res.status(500).send()
-    })
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({})
+    res.send(users)
+  } catch (e) {
+    res.status(500).send()
+  }
+  // // Fetch all users via promise that find returns
+  // User.find({})
+  //   .then(users => {
+  //     res.status(200).send(users)
+  //   })
+  //   .catch(error => {
+  //     res.status(500).send()
+  //   })
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id
-  // Mongoose automatically converts the ID so that we don't have to convert it for mongo
-  User.findById(_id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send()
-      }
-      res.status(200).send(user)
-    })
-    .catch(error => {
-      res.status(500).send(error)
-    })
+
+  try {
+    const user = await User.findById(_id)
+
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
+  } catch (e) {
+    res.status(500).send()
+  }
+  // // Mongoose automatically converts the ID so that we don't have to convert it for mongo
+  // User.findById(_id)
+  //   .then(user => {
+  //     if (!user) {
+  //       return res.status(404).send()
+  //     }
+  //     res.status(200).send(user)
+  //   })
+  //   .catch(error => {
+  //     res.status(500).send(error)
+  //   })
 })
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task)
-    })
-    .catch(error => {
-      res.status(400).send(error)
-    })
+
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+  // task
+  //   .save()
+  //   .then(() => {
+  //     res.status(201).send(task)
+  //   })
+  //   .catch(error => {
+  //     res.status(400).send(error)
+  //   })
 })
 
-app.get('/tasks', (req, res) => {
-  Task.find({})
-    .then(tasks => {
-      res.status(200).send(tasks)
-    })
-    .catch(error => {
-      res.status(500).send(error)
-    })
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.send(tasks)
+  } catch (e) {
+    res.status(500).send()
+  }
+  // Task.find({})
+  //   .then(tasks => {
+  //     res.status(200).send(tasks)
+  //   })
+  //   .catch(error => {
+  //     res.status(500).send(error)
+  //   })
 })
 
 app.get('/tasks/:id', (req, res) => {
