fc3cd83b64ee9c8d5a90062dafacc197b11e382d
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-17 19:58:44 -0600

    Added uniqueness to email

--- 4-task-manager/src/models/user.js
+++ 4-task-manager/src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -45,7 +46,25 @@ const userSchema = new mongoose.Schema({
   }
 })
 
+// Created our own custom function on the schema
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    throw new Error('Unable to login')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
+
+  return user
+}
+
 // Add middleware for encryption
+// Hash the plain text password before saving
 userSchema.pre('save', async function(next) {
   // we don't use arrow function because it doesn't bind to this
   const user = this
