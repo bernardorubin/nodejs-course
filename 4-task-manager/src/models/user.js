a01417f4f497570a7e2a036b06e8ccd044665269
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-17 19:28:25 -0600

    Store passwords safely in db

--- 4-task-manager/src/models/user.js
+++ 4-task-manager/src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -48,6 +49,9 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function(next) {
   // we don't use arrow function because it doesn't bind to this
   const user = this
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
   console.log('just before saving!')
   next()
 })
